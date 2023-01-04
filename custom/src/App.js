0319811e6b84ec8f9bd98270f98b07f792923969
Ajit Kumar Pandey <87422047+Ajit24@users.noreply.github.com>
2023-01-04 12:41:47 +0530

    custom hook updtated

--- custom/src/App.js
+++ custom/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import useFetch from './useFetch/useFetch';
 
 function App() {
+
+  const { data: joke, loading, error, refetch } = useFetch(
+    "https://v2.jokeapi.dev/joke/Any"
+  );
+
+  if (loading) return <h1> LOADING...</h1>;
+
+  if (error) console.log(error);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Custom - Hooks in React.js</h2> <hr />
+     <div>
+     <h1>
+        {joke?.setup} : {joke?.delivery}
+      </h1>
+
+      <button onClick={refetch}> Refetch</button>
+     </div>
     </div>
   );
 }
