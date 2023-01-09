b5a31f1089aa362a910bebb6990f4045d5730298
Ajit Kumar Pandey <87422047+Ajit24@users.noreply.github.com>
2023-01-09 11:11:28 +0530

    dark mode with login page

--- darkMode/darkmodee/src/App.js
+++ darkMode/darkmodee/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import DarkMode from './Components/DarkMode/DarkMode';
+import Nav from './Components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      {/* <Nav/> */}
+     <DarkMode/>
      
     </div>
   );
---
+++ darkMode/darkmodee/src/Components/Nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Nav = () => {
+
+
+
+  return (
+    <div>
+<nav className='nav' style={{height:'50px', background:'aqua'}}>
+       
+</nav>
+    </div>
+  )
+}
+
+export default Nav
