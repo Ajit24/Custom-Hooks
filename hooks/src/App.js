ec18a90e8a9525f3a4989e8776bb4f295c6bc571
Ajit Kumar Pandey <87422047+Ajit24@users.noreply.github.com>
2023-01-16 10:18:06 +0530

    use effects hooks

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -4,14 +4,16 @@ import HookCounterArr from './Components/HookCounterArr';
 import UseEffectOne from './Components/UseEffectOne';
 import DarkMode from './Components/DarkMode/DarkMode';
 import Header from './Components/Header/Header';
+import UseEffectFetch from './Components/UseEffectFetch';
 function App() {
   return (
     <div className="App">
      {/* <ClassCounter/> */}
      {/* <HookCounterArr/> */}
-     <UseEffectOne/>
+     {/* <UseEffectOne/> */}
      {/* <Header/> */}
      {/* <DarkMode/> */}
+     <UseEffectFetch/>
          
 
      
---
+++ hooks/src/Components/UseEffectFetch.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UseEffectFetch = () => {
+  return (
+    <div>
+      <h3>useEffect Fetch- </h3> <hr />
+
+
+
+      
+
+    </div>
+  )
+}
+
+export default UseEffectFetch
