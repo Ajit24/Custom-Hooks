65f5db14e9e0432e94c2e3dbfce095e6e507321f
Ajit Kumar Pandey <87422047+Ajit24@users.noreply.github.com>
2023-01-17 16:58:32 +0530

    login form with mui

--- darkMode/map/src/App.js
+++ darkMode/map/src/App.js
@@ -1,36 +1,36 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import Map from './Components/Map';
-// import LoginForm from './Components/LoginForm/LoginForm';
+import logo from './logo.svg';
+import './App.css';
+import Map from './Components/Map';
+import LoginForm from './Components/LoginForm/LoginForm';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <Map/> */}
-//       <LoginForm/>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      {/* <Map/> */}
+      <LoginForm/>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
 
 
 //import LoginForm from './LoginForm';
-import LoginForm from "./Components/LoginForm/LoginForm";
+// import LoginForm from "./Components/LoginForm/LoginForm";
 
-function App() {
-  const handleLogin = (data) => {
-    // Send a request to the server for authentication
-    console.log(data);
-  };
+// function App() {
+//   const handleLogin = (data) => {
+//     // Send a request to the server for authentication
+//     console.log(data);
+//   };
 
-  return (
-    <div>
-      <LoginForm onSubmit={handleLogin} />
-    </div>
-  );
-}
-export default App;
+//   return (
+//     <div>
+//       <LoginForm onSubmit={handleLogin} />
+//     </div>
+//   );
+// }
+// export default App;
 
 //AIzaSyAXN9QcZXsmPdZYao0EIEXXPQXpoR-62Gs
--- darkMode/map/src/Components/LoginForm/LoginForm.jsx
+++ darkMode/map/src/Components/LoginForm/LoginForm.jsx
@@ -39,9 +39,60 @@
 // export default LoginForm;
 
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function LoginForm({ onSubmit }) {
+// function LoginForm({ onSubmit }) {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     onSubmit({ username, password });
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label>
+//         Username:
+//         <input 
+//           type="text" 
+//           value={username} 
+//           onChange={(event) => setUsername(event.target.value)} 
+//         />
+//       </label>
+//       <br />
+//       <label>
+//         Password:
+//         <input 
+//           type="password" 
+//           value={password} 
+//           onChange={(event) => setPassword(event.target.value)} 
+//         />
+//       </label>
+//       <br />
+//       <button type="submit">Login</button>
+//     </form>
+//   );
+// }
+
+// export default LoginForm;
+
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+export default function LoginForm({ onSubmit }) {
+  const classes = useStyles();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -51,29 +102,27 @@ function LoginForm({ onSubmit }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input 
-          type="text" 
-          value={username} 
-          onChange={(event) => setUsername(event.target.value)} 
-        />
-      </label>
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <TextField
+        label="Username"
+        variant="outlined"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
       <br />
-      <label>
-        Password:
-        <input 
-          type="password" 
-          value={password} 
-          onChange={(event) => setPassword(event.target.value)} 
-        />
-      </label>
+      <TextField
+        label="Password"
+        variant="outlined"
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
       <br />
-      <button type="submit">Login</button>
+      <Button variant="contained" color="primary" type="submit">
+        Login
+      </Button>
     </form>
   );
 }
 
-export default LoginForm;
 
